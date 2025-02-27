aa75faf6f98d2577c4dee6f4607b16dad69528f1
killer_bigpoint <mineworkgaminglive@gmail.com>
2025-02-27 10:58:47 +0100

    Progress on better aimbot detection

--- Handlers/VirtualFuncs.cs
+++ Handlers/VirtualFuncs.cs
@@ -2,6 +2,7 @@
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Modules.Memory;
 using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+using CounterStrikeSharp.API.Modules.Utils;
 using TBAntiCheat.Core;
 
 namespace TBAntiCheat.Handlers
@@ -10,17 +11,24 @@ namespace TBAntiCheat.Handlers
     {
         internal static void Initialize()
         {
-            VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Hook(OnDamageTaken, HookMode.Pre);
+            //VirtualFunctions.CBaseEntity_TakeDamageOldFunc.Hook(OnDamageTaken, HookMode.Post);
 
             ACCore.Log($"[TBAC] VirtualFuncs Initialized");
         }
 
         private static HookResult OnDamageTaken(DynamicHook hook)
         {
+            CEntityInstance entityInfo = hook.GetParam<CEntityInstance>(0);
             CTakeDamageInfo damageInfo = hook.GetParam<CTakeDamageInfo>(1);
-            PlayerData player = Globals.Players[damageInfo.Attacker.Index];
 
-            Server.PrintToChatAll($"Damage Taken -> {damageInfo.GetHitGroup()} | {player.Controller.PlayerName}");
+            PlayerData victim = Globals.Players[entityInfo.Index];
+            PlayerData attacker = Globals.Players[damageInfo.Attacker.Index];
+
+            Vector damagePosEngine = damageInfo.DamagePosition;
+            System.Numerics.Vector3 dmgPos = new System.Numerics.Vector3(damagePosEngine.X, damagePosEngine.Y, damagePosEngine.Z);
+            System.Numerics.Vector3 eyeAngle = new System.Numerics.Vector3(attacker.Pawn.EyeAngles.X, attacker.Pawn.EyeAngles.Y, attacker.Pawn.EyeAngles.Z);
+
+            Server.PrintToChatAll($"Damage Taken -> {attacker.Controller.PlayerName} -> {victim.Controller.PlayerName} | HitGroup: {damageInfo.GetHitGroup()} | DamagePosition: {dmgPos} | DamageDirection: {damageInfo.DamageDirection} | {eyeAngle}");
 
             return HookResult.Continue;
         }
